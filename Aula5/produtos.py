6095fa1235580b432e2b208d1b79d81508775609
unknown <900133@proway.treina>
2019-11-13 08:21:39 -0300

    Aula5

--- Aula5/produtos.py
+++ Aula5/produtos.py
@@ -1,4 +1,12 @@
 nomep = input('Digite o nome do produto: ')
-categoria = int(input('Infome a categoria do produto'))
+pcat1 = ('Skol', 'Bhrama', 'Bohemia', 'Original', 'Heineken')
+pcat2 = ('Guaraná Antártica', 'Gatorade', 'Chá Lipton')
 
-#if categoria == 1 and idade >= 18?
+
+if categoria == 1 and idade >= 18:
+    print = int(input('Digite 1 para alcoolicos e 2 para não alcolicos')
+    bebida = int(input('Infome a categoria do produto'))
+'''if bebida == 1
+print ('produtos: /n: sss(1), ssss(2), sss')
+if bebida == 1:
+    print = ('cerveja selecionada')
