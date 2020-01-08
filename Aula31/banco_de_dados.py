48d934d6581154827067dd83132eca21c9fc094f
Bruna_idbruh <brunaalinedocarmo@gmail.com>
2020-01-08 11:50:03 -0300

    Aula31

---
+++ Aula31/banco_de_dados.py
@@ -0,0 +1,11 @@
+#   BANCO DE DADOS
+#   08/01/2019
+# SGBD: sistema gerenciador de banco de dados
+# MYSQL, SQLSERVER...
+# MYSQL = MARIADB
+# 
+# CRUD
+# C = CREATE  -- INERT
+# R = READ --  SELECT
+# U = UPDATE -- UPDATE
+# D = DELETE -- DELETE
