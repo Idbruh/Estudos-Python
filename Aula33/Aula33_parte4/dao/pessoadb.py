7e00d81fe7309f13ccfd49ddeea21696ba3da837
Bruna_idbruh <brunaalinedocarmo@gmail.com>
2020-01-15 11:49:35 -0300

    Aula33

--- Aula33/Aula33_parte4/dao/pessoadb.py
+++ Aula33/Aula33_parte4/dao/pessoadb.py
@@ -1,16 +1,5 @@
 import MySQLdb
-
-class Pessoa:  # primeira letra sempre maiuscula
-    id = 0  # assim como dicionario, iniciar sempre com valores padroes. Abaixo está a definiçao da classe Pessoa
-    nome = ''
-    sobrenome = ''
-    idade = 0
-    enereco_id = 0
-    
-    def exportar_txt(self,lista_pessoas):
-        with open('Aula33_parte4/pessoas.txt','a') as arquivo: 
-            for p in lista_pessoas:
-                arquivo.write(f"{p.id};{p.nome};{p.sobrenome};{p.idade};{p.endereco_id};\n")
+from model.pessoa import Pessoa
 
 
 class PessoaDb:
@@ -44,7 +33,3 @@ class PessoaDb:
         return lista_pessoas
 
 
-
-
-    def __str__(self):
-        return f"{self.id};{self.nome};{self.sobrenome};{self.idade};{self.endereco_ID}"
--- Aula33/Aula33_parte4/model/pessoa.py
+++ Aula33/Aula33_parte4/model/pessoa.py
@@ -9,6 +9,10 @@ class Pessoa:  # primeira letra sempre maiuscula
     enereco_id = 0
     
     def exportar_txt(self,lista_pessoas):
-        with open('Aula33_parte4/pessoas10.txt','a') as arquivo: 
+        with open('pessoas10.txt','a') as arquivo: 
             for p in lista_pessoas:
                 arquivo.write(f"{p.id};{p.nome};{p.sobrenome};{p.idade};{p.endereco_id};\n")
+    
+
+    def __str__(self):
+        return f"{self.id};{self.nome};{self.sobrenome};{self.idade};{self.endereco_ID}"
